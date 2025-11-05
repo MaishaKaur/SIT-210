2e49d78604516ef4c3771ba0a86c05208456214e
MaishaKaur <187090128+MaishaKaur@users.noreply.github.com>
2025-11-06 01:37:51 +0530

    Update 2.1p.ino

--- Task2.1WebHook/2.1p.ino
+++ Task2.1WebHook/2.1p.ino
@@ -1,28 +1,28 @@
-#include <SPI.h>
-#include <WiFiNINA.h>
-#include <DHT.h>
+#include <SPI.h>              // Enables SPI communication for Wi-Fi module
+#include <WiFiNINA.h>         // Enables Wi-Fi functions (connect, send data)
+#include <DHT.h>              // Enables temperature and humidity sensor support
 
-#define DHTPIN 2
-#define DHTTYPE DHT22
+#define DHTPIN 2              // DHT sensor data pin connected to D2
+#define DHTTYPE DHT22         // Using DHT22 sensor model
 
-char ssid[] = "1313";
-char pass[] = "1234567a";
+char ssid[] = "1313";         // Wi-Fi network name
+char pass[] = "1234567a";     // Wi-Fi password
 
-String apiKey = "DCDDKSPB88JM2K3V";
-const char* server = "api.thingspeak.com";
+String apiKey = "DCDDKSPB88JM2K3V";          // ThingSpeak channel write key
+const char* server = "api.thingspeak.com";   // ThingSpeak server address
 
-WiFiClient client;
-DHT dht(DHTPIN, DHTTYPE);
+WiFiClient client;            // Wi-Fi client for HTTP requests
+DHT dht(DHTPIN, DHTTYPE);     // DHT sensor object
 
-unsigned long lastTime = 0;
-unsigned long interval = 16000;
+unsigned long lastTime = 0;   // Stores last data send time
+unsigned long interval = 16000; // Send every 16 seconds
 
 void setup() {
-  Serial.begin(9600);
-  dht.begin();
-
+  Serial.begin(9600);         // Start serial monitor
+  dht.begin();                // Start DHT sensor
+  
   Serial.print("Connecting to WiFi");
-  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) { // Connect to Wi-Fi
     Serial.print(".");
     delay(1000);
   }
@@ -30,43 +30,27 @@ void setup() {
 }
 
 void loop() {
-  if (millis() - lastTime > interval) {
-    float temperature = dht.readTemperature();
+  if (millis() - lastTime > interval) {       // Run every 16 seconds
+    float temperature = dht.readTemperature(); // Read temperature (°C)
 
-    if (isnan(temperature)) {
-      Serial.println("Failed to read from DHT sensor!");
-      return;
-    }
+    String postData = "api_key=" + apiKey + "&field1=" + String(temperature); // Format data
 
-    Serial.print("Temperature: ");
-    Serial.print(temperature);
-    Serial.println(" °C");
-
-    if (client.connect(server, 80)) {
-      String postData = "api_key=" + apiKey + "&field1=" + String(temperature);
-
-      client.println("POST /update HTTP/1.1");
+    if (client.connect(server, 80)) {         // Connect to ThingSpeak server
+      client.println("POST /update HTTP/1.1");  // HTTP POST request
       client.println("Host: api.thingspeak.com");
       client.println("Connection: close");
       client.println("Content-Type: application/x-www-form-urlencoded");
       client.print("Content-Length: ");
       client.println(postData.length());
       client.println();
-      client.println(postData);
+      client.println(postData);               // Send data payload
 
-      Serial.println("Data sent to ThingSpeak!");
-
-      while (client.connected() && !client.available()) delay(1);
-      while (client.available()) {
-        String line = client.readStringUntil('\n');
-        Serial.println("ThingSpeak response: " + line);
-      }
-
-    } else {
-      Serial.println("Connection to ThingSpeak failed.");
+      Serial.print("Temperature: ");          // Print confirmation
+      Serial.print(temperature);
+      Serial.println(" °C sent to ThingSpeak!");
     }
 
-    client.stop();
-    lastTime = millis();
+    client.stop();                            // Close connection
+    lastTime = millis();                      // Update timer
   }
 }
